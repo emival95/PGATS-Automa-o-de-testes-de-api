3b97bf8607d84ba3bba72f455752d4acf7eae2bb
emival95 <emivalferreira38@gmail.com>
2025-08-09 17:57:31 -0300

    adiconando teste mocado

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -6,6 +6,9 @@ const { expect } = require('chai');
 // Aplicação
 const app = require('../../app');
 
+//mock
+const tranferService = require('../../service/transferService');
+
 // Testes
 describe('TransferController', () => {
     describe('POST /transfer', () => {
@@ -19,6 +22,28 @@ describe('TransferController', () => {
 
                 });
              expect(resposta.status).to.equal(400);
+             expect(resposta.body).to.property('error', 'Usuário não encontrado');
+             
+        });
+
+
+         it('Usando Mokcs: Quando informo remetente e destinatario inexistentes recebo 400', async () => {
+           // mocar apenas a função do tranfer service
+           const tranferServiceMock = sinon.stub(tranferService, 'transfer');
+           tranferServiceMock.throws(new Error('Usuário não encontrado'));
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    from:"emival",
+                    to:"valzin",
+                    value:100
+
+                });
+             expect(resposta.status).to.equal(400);
+             expect(resposta.body).to.property('error', 'Usuário não encontrado');
+
+             // reseto o Mock
+             sinon.restore();
              
         });
 
