736cc57964a068412e3b50bd29f930a943be9405
emival95 <emivalferreira38@gmail.com>
2025-08-09 16:56:20 -0300

    corrigindo

--- service/transferService.js
+++ service/transferService.js
@@ -4,7 +4,7 @@ const transferRepo = require('../repository/transferRepository');
 function transfer({ from, to, value }) {
   const sender = userRepo.findUser(from);
   const recipient = userRepo.findUser(to);
-  if (!sender || !recipient) throw new Error('Usuário,  encontrado');
+  if (!sender || !recipient) throw new Error('Usuário não encontrado');
   if (sender.saldo < value) throw new Error('Saldo insuficiente');
   const isFavorecido = sender.favorecidos.includes(to);
   if (!isFavorecido && value >= 5000) throw new Error('Transferência acima de R$ 5.000,00 só para favorecidos');
